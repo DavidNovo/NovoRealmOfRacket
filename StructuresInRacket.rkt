50c81615e48c6dfa45b5f88422d6a25ff7f815d8
Unknown <david.novogrodsky@gmail.com>
2021-10-24 22:31:46 -0500

    more experiments

--- StructuresInRacket.rkt
+++ StructuresInRacket.rkt
@@ -18,3 +18,15 @@
 
 (struct example2 (p q r) #:transparent)
 (define ex2 (example2 9 8 7))
+;; this is transparent so this happens
+;; > example2
+;; #<procedure:example2>
+
+
+(struct student-body (freshman sophomores juniors seniors))
+
+(define all-students
+    (student-body (list freshman1 (student 'Mary 0101 'OldHall))
+                  	(list (student 'Jeff 5678 'OldHall))
+                  	(list (student 'Bod 4321 'Apartment))
+                  empty))
